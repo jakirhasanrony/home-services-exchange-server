1c0e767d5978d3af08a374672b6f7896beeaf075
Md. Jakir Hasan Rony <jakir15-14584@diu.edu.bd>
2023-11-09 18:53:20 +0600

    jwt privacy and verification functionalities added: all updated for this project

--- index.js
+++ index.js
@@ -32,6 +32,30 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middlewares
+const logger = (req, res, next) => {
+  console.log(('log info: '), req.method, req.url);
+  next();
+}
+
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  // console.log('token in the middleware', token);
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  // next();
+  jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded;
+    next();
+  })
+
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -43,7 +67,7 @@ async function run() {
     const bookedCollection = client.db('homeServicesExchange').collection('bookings');
 
     // auth related api
-    app.post('/jwt', async (req, res) => {
+    app.post('/jwt', logger, async (req, res) => {
       const user = req.body;
       console.log('user for token:', user);
       const token = jwt.sign(user,
@@ -145,8 +169,13 @@ async function run() {
 
     })
 
-    app.get('/bookings', async (req, res) => {
+    app.get('/bookings', logger, verifyToken, async (req, res) => {
       // console.log(req.query.email);
+      console.log('token owner:', req.user);
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email }
